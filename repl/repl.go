476b7f7efee33b3a308a795dc787cf2aeca34094
conurfloki <conurfloki@gmail.com>
2023-11-27 21:17:07 -0300

    ADD - object type system and evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/conur-floki/gomonkey/evaluator"
 	"github.com/conur-floki/gomonkey/lexer"
 	"github.com/conur-floki/gomonkey/parser"
 )
@@ -17,7 +18,6 @@ func Start(in io.Reader, out io.Writer) {
 
 	for {
 		fmt.Print(PROMPT)
-
 		scanned := scanner.Scan()
 		if !scanned {
 			return
@@ -33,8 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
